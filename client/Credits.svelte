c931c3d1f4d61e4644fed917bfff131f0cac7fd6
David Johansson <david.johansson@aptiv.com>
2020-07-28 21:12:53 +0200

    Added some credits

--- client/Credits.svelte
+++ client/Credits.svelte
@@ -11,3 +11,30 @@
 </script>
 
 <h1>Credits</h1>
+<div>
+    <p>
+        <b>Meteor is an open source platform for web, mobile, and
+        desktop used by over half a million developers around the 
+        globe to make shipping javascript applications simple, 
+        efficient, and scalable.</b><br/>
+        > <a href="https://www.meteor.com/">Meteor</a> - MIT License
+    </p>
+    <p>
+        <b>Cybernetically enhanced web apps</b><br/>
+        > <a href="https://svelte.dev/">Svelte</a> -MIT License
+    </p>
+    <p>
+        <b>MongoDB is a document database with the scalability and 
+        flexibility that you want with the querying and indexing that 
+        you need</b><br/>
+        > <a href="https://www.mongodb.com/">Mongo DB</a> - Server Side Public License (SSPL) v1
+    </p>
+    <p>
+        <b>Lightweight and beautiful library</b><br/>
+        > <a href="https://picnicss.com/">Picnic CSS</a> - MIT License
+    </p>
+    <p>
+        <b>A SSL Proxy for Meteor</b><br/>
+        > <a href="https://github.com/Tarang/Meteor-SSL-proxy">SSL Proxy for Meteor</a> - Unknown
+    </p>
+</div>
